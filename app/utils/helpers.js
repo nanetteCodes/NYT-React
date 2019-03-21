12e72ca4bfa4b48075bb512b45f49ad09759cdf6
Nanette <njulius89@gmail.com>
2019-03-21 09:05:37 -0700

    updated api key

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -2,67 +2,63 @@
 var axios = require("axios");
 
 // NYT API Key (Replace with your own API Key)
-var APIKey = "9b3adf57854f4a19b7b5782cdd6e427a";
+var APIKey = "cujFG4ALQLdytAb1Qr7Ccn9QjR5IATv7";
 
 // Helper Functions
 var helpers = {
-
   // This will run our query.
   runQuery: function(term, start, end) {
-
     // Adjust to get search terms in proper format
     var formattedTerm = term.trim();
     var formattedStart = start.trim() + "0101";
     var formattedEnd = end.trim() + "1231";
 
-
     console.log("Query Run");
 
-    return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
-      params: {
-        "api-key": APIKey,
-        "q": formattedTerm,
-        "begin_date": formattedStart,
-        "end_date": formattedEnd
-      }
-    })
-    .then(function(results) {
-      console.log("Axios Results", results.data.response);
-      return results.data.response;
-    });
+    return axios
+      .get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
+        params: {
+          "api-key": APIKey,
+          q: formattedTerm,
+          begin_date: formattedStart,
+          end_date: formattedEnd
+        }
+      })
+      .then(function(results) {
+        console.log("Axios Results", results.data.response);
+        return results.data.response;
+      });
   },
 
   getSaved: function() {
-    return axios.get("/api/saved")
+    return axios.get("/api/saved").then(function(results) {
+      console.log("axios results", results);
+      return results;
+    });
+  },
+  postSaved: function(title, date, url) {
+    var newArticle = { title: title, date: date, url: url };
+    return axios.post("/api/saved", newArticle).then(function(response) {
+      console.log("axios results", response.data._id);
+      return response.data._id;
+    });
+  },
+
+  deleteSaved: function(title, data, url) {
+    return axios
+      .delete("/api/saved", {
+        params: {
+          title: title,
+          data: data,
+          url: url
+        }
+      })
       .then(function(results) {
         console.log("axios results", results);
         return results;
       });
-  },
-  postSaved: function(title, date, url) {
-    var newArticle = { title: title, date: date, url: url };
-    return axios.post("/api/saved", newArticle)
-      .then(function(response) {
-        console.log("axios results", response.data._id);
-        return response.data._id;
-      });
-  },
-
-  deleteSaved: function(title, data, url) {
-    return axios.delete("/api/saved", {
-      params: {
-        "title": title,
-        "data": data,
-        "url": url
-      }
-    })
-    .then(function(results) {
-      console.log("axios results", results);
-      return results;
-    });
   }
 };
-
 
 // We export the helpers function
 module.exports = helpers;
